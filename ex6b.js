5e462817ca534c7911c582f555be798398f398e8
Yago Riveiro <yago.riveiro@gmail.com>
2014-03-08 16:36:09 +0000

    Identation fixes.

--- ex6b.js
+++ ex6b.js
@@ -4,7 +4,8 @@ var dir = process.argv[2];
 var ext = process.argv[3];
 
 filter(dir, ext, function(err, data){
-    if (err) throw err;
+    if (err)
+        throw err;
 
     data.forEach(function(file) {
         console.log(file);
